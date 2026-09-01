d959024dea2aa2362521498b7db283bfa36aea81
Lucas Marinho <lucassenatore4@gmail.com>
2026-09-01 14:55:23 -0300

    🐛fix: endPoint de veiculos

--- backend/src/controllers/veiculoController.js
+++ backend/src/controllers/veiculoController.js
@@ -1,227 +1,62 @@
-const pool = require("../config/database");
+import { veiculoService } from "../services/veiculoServices";
 
-// LISTAR
-async function listarVeiculos(req, res) {
-  try {
-    const result = await pool.query(`
-      SELECT 
-        v.*,
-        u.nome AS usuario_nome
-      FROM veiculos v
-      JOIN usuarios u ON u.id = v.usuario_id
-      ORDER BY v.id DESC
-    `);
-
-    res.json(result.rows);
-  } catch (error) {
-  console.error("ERRO AO LISTAR VEÍCULOS:", error);
-
-  res.status(500).json({
-    erro: error.message
-  });
-}
-}
-
-// BUSCAR POR ID
-async function buscarVeiculo(req, res) {
-  try {
-    const { id } = req.params;
-
-    const result = await pool.query(
-      `
-      SELECT 
-        v.*,
-        u.nome AS usuario_nome
-      FROM veiculos v
-      JOIN usuarios u ON u.id = v.usuario_id
-      WHERE v.id = $1
-      `,
-      [id]
-    );
-
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        erro: "Veículo não encontrado",
-      });
-    }
-
-    res.json(result.rows[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ erro: "Erro ao buscar veículo" });
-  }
-}
-
-// CADASTRAR
-async function criarVeiculo(req, res) {
-  try {
-    const {
-      usuario_id,
-      marca,
-      modelo,
-      ano,
-      quilometragem,
-      preco,
-      cor,
-      combustivel,
-      cambio,
-      categoria,
-      descricao,
-      imagem,
-      status,
-    } = req.body;
-
-    const result = await pool.query(
-      `
-      INSERT INTO veiculos (
-        usuario_id,
-        marca,
-        modelo,
-        ano,
-        quilometragem,
-        preco,
-        cor,
-        combustivel,
-        cambio,
-        categoria,
-        descricao,
-        imagem,
-        status
+export const veiculoController = {
+  async getAll(req, res){
+    try {
+      const getAll = await veiculoService.getAll()
+      res.status(200).json(getAll)
+    } catch (error) {
+      res.status(500).json(
+        {erro: error.message}
       )
-      VALUES (
-        $1, $2, $3, $4, $5, $6, $7,
-        $8, $9, $10, $11, $12, $13
+    }
+  },
+  async getById(req, res){
+    try {
+      const getById = await veiculoService.getById(req.params.id)
+      res.status(200).json(getById)
+    } catch (error) {
+      res.status(500).json(
+        {erro: error.message}
       )
-      RETURNING *
-      `,
-      [
-        usuario_id,
-        marca,
-        modelo,
-        ano,
-        quilometragem,
-        preco,
-        cor,
-        combustivel,
-        cambio,
-        categoria,
-        descricao,
-        imagem,
-        status || "Disponível",
-      ]
-    );
-
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ erro: "Erro ao cadastrar veículo" });
+    }
+  },
+  async create(req, res){
+    try {
+      const novoVeiculo = await veiculoService.create(req.body);
+      res.status(201).json(novoVeiculo)
+    } catch (error) {
+      res.status(400).json(
+        {erro: error.message}
+      )
+    }
+  },
+  async updateVeiculo(req, res){
+    try {
+      const veiculoAtualizado = await veiculoService.update(req.params.id, req.body)
+      res.status(200).json(veiculoAtualizado)
+    } catch (error) {
+      const status = error.message === 'Veiculo não encontrado!' ? 404 : 400;
+      res.status(status).json(
+        {erro: error.message}
+      )
+    }
+  },
+  async delete(req, res){
+    try {
+      const veiculo = await veiculoService.getById(req.params.id)
+      if(!veiculo){
+        return res.status(404).json(
+          {erro: "post não encontrado!"}
+        )
+      }
+      const veiculoDeletado = await veiculoService.delete(req.params.id)
+      res.status(200).json(veiculoDeletado) 
+    } catch (error) {
+      const status = error.message === "veiculo não econtrado!" ? 404 : 400;
+      res.status(status).json(
+        {erro: error.message}
+      )
+    }
   }
 }
-
-// ATUALIZAR
-async function atualizarVeiculo(req, res) {
-  try {
-    const { id } = req.params;
-
-    const {
-      usuario_id,
-      marca,
-      modelo,
-      ano,
-      quilometragem,
-      preco,
-      cor,
-      combustivel,
-      cambio,
-      categoria,
-      descricao,
-      imagem,
-      status,
-    } = req.body;
-
-    const result = await pool.query(
-      `
-      UPDATE veiculos
-      SET
-        usuario_id = $1,
-        marca = $2,
-        modelo = $3,
-        ano = $4,
-        quilometragem = $5,
-        preco = $6,
-        cor = $7,
-        combustivel = $8,
-        cambio = $9,
-        categoria = $10,
-        descricao = $11,
-        imagem = $12,
-        status = $13
-      WHERE id = $14
-      RETURNING *
-      `,
-      [
-        usuario_id,
-        marca,
-        modelo,
-        ano,
-        quilometragem,
-        preco,
-        cor,
-        combustivel,
-        cambio,
-        categoria,
-        descricao,
-        imagem,
-        status,
-        id,
-      ]
-    );
-
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        erro: "Veículo não encontrado",
-      });
-    }
-
-    res.json(result.rows[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ erro: "Erro ao atualizar veículo" });
-  }
-}
-
-// EXCLUIR
-async function excluirVeiculo(req, res) {
-  try {
-    const { id } = req.params;
-
-    const result = await pool.query(
-      "DELETE FROM veiculos WHERE id = $1 RETURNING id",
-      [id]
-    );
-
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        erro: "Veículo não encontrado",
-      });
-    }
-
-    res.json({
-      mensagem: "Veículo excluído com sucesso",
-    });
-  } catch (error) {
-  console.error("ERRO AO CADASTRAR:", error);
-
-  res.status(500).json({
-    erro: error.message
-  });
-}
-}
-
-module.exports = {
-  listarVeiculos,
-  buscarVeiculo,
-  criarVeiculo,
-  atualizarVeiculo,
-  excluirVeiculo,
-};
---
+++ backend/src/repositories/veiculoRepositories.js
@@ -0,0 +1,28 @@
+import { query } from "../config/database";
+
+export const veiculoRepository = {
+    async findAll(){
+        const res = await query('SELECT * FROM veiculos');
+        return res.rows
+    },
+    async findById(id){
+        const res = await query('SELECT * FROM veiculos WHERE id = $1', [id])
+        return res.rows[0]
+    },
+    async create(veiculo){
+        const { marca, modelo, ano, quilometragem, preco, cor, combustivel, cambio, categoria, descricao, imagem } = veiculo;
+        const sql = 'INSERT INTO veiculos (usuario_id, marca, modelo, ano, quilometragem, preco, cor, combustivel, cambio, categoria, descricao, imagem) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) returning *';
+        const res = await query(sql, [marca, modelo, ano, quilometragem, preco, cor ,combustivel, cambio, categoria, descricao, imagem])
+    },
+    async update(id, veiculo){
+        const {quilometragem, preco, cor, combustivel } = veiculo
+        const sql = 'UPDATE veiculos SET quilometragem = $1, preco = $2, cor = $3, combustivel = $4, usuario_id = $5 returning *;';
+        const res = await query(sql, [quilometragem, preco, cor, combustivel], id)
+        return res.rows[0]
+    },
+    async delete(id){
+        const sql = 'DELETE FROM veiculos WHERE id = $1 returning *';
+        const res = await query(sql, [id]);
+        return res.rows[0]
+    }
+}
